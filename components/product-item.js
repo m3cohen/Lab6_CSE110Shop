ee1c7d32788d83e2011736ef994cb9bbd3b7d1d5
m3cohen <60684461+m3cohen@users.noreply.github.com>
2021-02-09 21:02:08 -0800

    Update product-item.js

--- components/product-item.js
+++ components/product-item.js
@@ -1,11 +1,25 @@
 // product-item.js
 
 class ProductItem extends HTMLElement {
-
-  <img src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" alt="Fjallraven - Foldstack No. 1 Backpack, Fits 15 Laptops" width=200>
-  <p class="title">Fjallraven - Foldstack No. 1 Backpack, Fits 15 Laptops</p>
-  <p class="price">$109.95</p>
-  <button onclick="alert('Added to Cart!')">Add to Cart</button>
+  constructor(){
+    super();
+    this.template = document.createElement('template');
+    this.template.innerHTML = ` `;
+    
+    this.attachShadow({mode: 'open'});
+    this.shadowRoot.appendChild(this.template.content.cloneNode(true));
+  }
+  
+  
+  
+  
+  
+  <!-- li class="product">
+    <img src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" alt="Fjallraven - Foldstack No. 1 Backpack, Fits 15 Laptops" width=200>
+    <p class="title">Fjallraven - Foldstack No. 1 Backpack, Fits 15 Laptops</p>
+    <p class="price">$109.95</p>
+    <button onclick="alert('Added to Cart!')">Add to Cart</button>
+ </li -->
                
 }
 
