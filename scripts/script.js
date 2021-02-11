2644964daffd187609c930783a9aa08adc5ae230
m3cohen <60684461+m3cohen@users.noreply.github.com>
2021-02-10 22:29:59 -0800

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
 // Script.js
-
+/*
  window.addEventListener('DOMContentLoaded', () => {
     fetch('https://fakestoreapi.com/products')
     .then(response => response.json())
@@ -19,3 +19,25 @@
       productList.appendChild(product);
   }
 });
+*/
+
+if (localStorage.getItem('items') === null) {
+  window.addEventListener('DOMContentLoaded', () => {
+    fetch('https://fakestoreapi.com/products')
+    .then(response => response.json())
+    .then(data => localStorage.setItem('items', JSON.stringify(data)));
+  });
+}
+
+let parsed = JSON.parse(localStorage.getItem('items'));
+let list = document.getElementById('product-list');
+
+for (let i = 0; i < parsed.length; i++) {
+  let item = parsed[i];
+  let product = document.createElement('product-item');
+  product.setAttribute('image', item.image);
+  product.setAttribute('title', item.title);
+  product.setAttribute('price', item.price);
+  product.setAttribute('id', item.id);
+  list.appendChild(product);
+}
