cf2749402efb1dd82e403a9f122a142abf96f3af
m3cohen <60684461+m3cohen@users.noreply.github.com>
2021-02-10 20:57:46 -0800

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,21 @@
 // Script.js
 
-window.addEventListener('DOMContentLoaded', () => {
-  let url = 'https://fakestoreapi.com/products';
-  let response = await fetch(url);
+ window.addEventListener('DOMContentLoaded', () => {
+    fetch('https://fakestoreapi.com/products')
+    .then(response => response.json())
+    .then(data => localStorage.setItem('items', JSON.stringify(data)));
+  });
   
-  myStorage = window.localStorage;
-  let array = localStorage.getItem(response);
-  
-  
+  let array = JSON.parse(localStorage.getItem('items'));
+  let productList = document.getElementById('product-list');
+
+  for (let i = 0; i < parsed.length; i++) {
+      let item = array[i];
+      let product = document.createElement('product-item');
+      product.setAttribute('image', item.image);
+      product.setAttribute('title', item.title);
+      product.setAttribute('price', item.price);
+      product.setAttribute('id', item.id);
+      productList.appendChild(product);
+  }
 });
