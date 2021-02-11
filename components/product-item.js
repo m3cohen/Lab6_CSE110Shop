ede238e01a9b8832f0f3e7efa214425e820eab8f
m3cohen <60684461+m3cohen@users.noreply.github.com>
2021-02-10 22:21:21 -0800

    Update product-item.js

--- components/product-item.js
+++ components/product-item.js
@@ -15,39 +15,37 @@ class ProductItem extends HTMLElement {
         product.setAttribute('class', 'product');
         
         image.setAttribute('src', this.getAttribute('image'));
-        image.setAttribute('src', this.getAttribute('image'));
         image.setAttribute('alt', this.getAttribute('title'));
         image.setAttribute('width', 200);
         image.setAttribute('class', 'pix');
       
-        name.textContent = this.getAttribute('title');
-        name.setAttribute('class', 'title');
+        title.textContent = this.getAttribute('title');
+        title.setAttribute('class', 'title');
         
         price.textContent = '$' + this.getAttribute('price');
         price.setAttribute('class', 'price');
       
-          let id = this.getAttribute('id');
-          let count = document.getElementById('cart-count');
-          if(localStorage.getItem(id)) {
+        let id = this.getAttribute('id');
+        let count = document.getElementById('cart-count');
+        if(localStorage.getItem(id)) {
+          button.textContent = 'Remove from Cart';
+          count.setAttribute('textContent', parseInt(count.textContent++));
+        } else {
+          button.textContent = 'Add to Cart';
+        }
+
+        button.onclick = function() {
+          if(button.textContent == 'Add to Cart') {     
+            count.setAttribute('textContent', parseInt(count.textContent++));
             button.textContent = 'Remove from Cart';
-            count.setAttribute('textContent', parseInt(++count.textContent));
+            localStorage.setItem(id, id);
           } else {
+            count.setAttribute('textContent', parseInt(count.textContent--));
             button.textContent = 'Add to Cart';
+            localStorage.removeItem(id);
           }
 
-          button.onclick = function() {
-            alert('Added to cart!'); 
-            if(button.textContent == 'Add to Cart') {     
-              count.setAttribute('textContent', parseInt(++count.textContent));
-              button.textContent = 'Remove from Cart';
-              localStorage.setItem(id, id);
-            } else {
-              count.setAttribute('textContent', parseInt(--count.textContent));
-              button.textContent = 'Add to Cart';
-              localStorage.removeItem(id);
-            }
-
-          };
+        };
         
       
       
@@ -58,67 +56,67 @@ class ProductItem extends HTMLElement {
         let style = document.createElement('style');
           style.textContent = `
             .price {
-              color: green;
-              font-size: 1.8em;
-              font-weight: bold;
-              margin: 0;
-            }
+            color: green;
+            font-size: 1.8em;
+            font-weight: bold;
+            margin: 0;
+          }
 
-            .product {
-              align-items: center;
-              background-color: white;
-              border-radius: 5px;
-              display: grid;
-              grid-template-areas: 
-              'image'
-              'title'
-              'price'
-              'add';
-              grid-template-rows: 67% 11% 11% 11%;
-              height: 450px;
-              filter: drop-shadow(0px 0px 6px rgb(0,0,0,0.2));
-              margin: 0 30px 30px 0;
-              padding: 10px 20px;
-              width: 200px;
-            }
+          .product {
+            align-items: center;
+            background-color: white;
+            border-radius: 5px;
+            display: grid;
+            grid-template-areas: 
+            'image'
+            'title'
+            'price'
+            'add';
+            grid-template-rows: 67% 11% 11% 11%;
+            height: 450px;
+            filter: drop-shadow(0px 0px 6px rgb(0,0,0,0.2));
+            margin: 0 30px 30px 0;
+            padding: 10px 20px;
+            width: 200px;
+          }
 
-            .product > button {
-              background-color: rgb(255, 208, 0);
-              border: none;
-              border-radius: 5px;
-              color: black;
-              justify-self: center;
-              max-height: 35px;
-              padding: 8px 20px;
-              transition: 0.1s ease all;
-            }
+          .product > button {
+            background-color: rgb(255, 208, 0);
+            border: none;
+            border-radius: 5px;
+            color: black;
+            justify-self: center;
+            max-height: 35px;
+            padding: 8px 20px;
+            transition: 0.1s ease all;
+          }
 
-            .product > button:hover {
-              background-color: rgb(255, 166, 0);
-              cursor: pointer;
-              transition: 0.1s ease all;
-            }
+          .product > button:hover {
+            background-color: rgb(255, 166, 0);
+            cursor: pointer;
+            transition: 0.1s ease all;
+          }
 
-            .product > img {
-              align-self: center;
-              justify-self: center;
-              width: 100%;
-            }
+          .product > img {
+            align-self: center;
+            justify-self: center;
+            width: 100%;
+          }
 
-            .title {
-              font-size: 1.1em;
-              margin: 0;
-              overflow: hidden;
-              text-overflow: ellipsis;
-              white-space: nowrap;
-            }
+          .title {
+            font-size: 1.1em;
+            margin: 0;
+            overflow: hidden;
+            text-overflow: ellipsis;
+            white-space: nowrap;
+          }
 
-            .title:hover {
-              font-size: 1.1em;
-              margin: 0;
-              white-space: wrap;
-              overflow: auto;
-              text-overflow: unset;
+          .title:hover {
+            font-size: 1.1em;
+            margin: 0;
+            white-space: wrap;
+            overflow: auto;
+            text-overflow: unset;
         }`;
 
 
