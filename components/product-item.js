2cbc617598004a64bee5546b6aa4e54083e16294
m3cohen <60684461+m3cohen@users.noreply.github.com>
2021-02-10 22:02:31 -0800

    Update product-item.js

--- components/product-item.js
+++ components/product-item.js
@@ -22,77 +22,78 @@ class ProductItem extends HTMLElement {
     
     
     let style = document.createElement('style');
-    
-    style.textContent = 
-      .price {
-        color: green;
-        font-size: 1.8em;
-        font-weight: bold;
-        margin: 0;
-      }
+      style.textContent = `
+        .price {
+          color: green;
+          font-size: 1.8em;
+          font-weight: bold;
+          margin: 0;
+        }
 
-      .product {
-        align-items: center;
-        background-color: white;
-        border-radius: 5px;
-        display: grid;
-        grid-template-areas: 
-        'image'
-        'title'
-        'price'
-        'add';
-        grid-template-rows: 67% 11% 11% 11%;
-        height: 450px;
-        filter: drop-shadow(0px 0px 6px rgb(0,0,0,0.2));
-        margin: 0 30px 30px 0;
-        padding: 10px 20px;
-        width: 200px;
-      }
+        .product {
+          align-items: center;
+          background-color: white;
+          border-radius: 5px;
+          display: grid;
+          grid-template-areas: 
+          'image'
+          'title'
+          'price'
+          'add';
+          grid-template-rows: 67% 11% 11% 11%;
+          height: 450px;
+          filter: drop-shadow(0px 0px 6px rgb(0,0,0,0.2));
+          margin: 0 30px 30px 0;
+          padding: 10px 20px;
+          width: 200px;
+        }
 
-      .product > button {
-        background-color: rgb(255, 208, 0);
-        border: none;
-        border-radius: 5px;
-        color: black;
-        justify-self: center;
-        max-height: 35px;
-        padding: 8px 20px;
-        transition: 0.1s ease all;
-      }
+        .product > button {
+          background-color: rgb(255, 208, 0);
+          border: none;
+          border-radius: 5px;
+          color: black;
+          justify-self: center;
+          max-height: 35px;
+          padding: 8px 20px;
+          transition: 0.1s ease all;
+        }
 
-      .product > button:hover {
-        background-color: rgb(255, 166, 0);
-        cursor: pointer;
-        transition: 0.1s ease all;
-      }
+        .product > button:hover {
+          background-color: rgb(255, 166, 0);
+          cursor: pointer;
+          transition: 0.1s ease all;
+        }
 
-      .product > img {
-        align-self: center;
-        justify-self: center;
-        width: 100%;
-      }
+        .product > img {
+          align-self: center;
+          justify-self: center;
+          width: 100%;
+        }
 
-      .title {
-        font-size: 1.1em;
-        margin: 0;
-        overflow: hidden;
-        text-overflow: ellipsis;
-        white-space: nowrap;
-      }
+        .title {
+          font-size: 1.1em;
+          margin: 0;
+          overflow: hidden;
+          text-overflow: ellipsis;
+          white-space: nowrap;
+        }
 
-      .title:hover {
-        font-size: 1.1em;
-        margin: 0;
-        white-space: wrap;
-        overflow: auto;
-        text-overflow: unset;
-      }
-    
-      shadow.appendChild(style);
-      shadow.appendChild(price);
-      shadow.appendChild(image);
-      shadow.appendChild(title);
-      shadow.appendChild(button);
+        .title:hover {
+          font-size: 1.1em;
+          margin: 0;
+          white-space: wrap;
+          overflow: auto;
+          text-overflow: unset;
+    }`;
+
+
+      this.shadowRoot.appendChild(style);
+      this.shadowRoot.appendChild(product);
+      product.appendChild(image);
+      product.appendChild(title);
+      product.appendChild(price);
+      product.appendChild(button);
      
     
 }
